2aa1be3bbd53285bebb15639d4e9541601fc9960
sk4900 <43387347+sk4900@users.noreply.github.com>
2020-03-02 22:43:17 -0500

    Refactored commands into the command pattern

---
+++ src/control/Commands/Command.java
@@ -0,0 +1,17 @@
+package control.Commands;
+
+import model.Playable;
+
+public class Command {
+    private Action action;
+    private Playable playable;
+
+    public Command(Action action, Playable playable) {
+        this.action = action;
+        this.playable = playable;
+    }
+    public void commandRequest(){
+        action.performAction(playable);
+    }
+}
+
